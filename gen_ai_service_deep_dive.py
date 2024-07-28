313e022f9376a8acfb9110038f9b1c7b63fff66c
ShraddhaAbhang <shraddhasampatabhang@gmail.com>
2024-07-28 23:13:07 +0530

    Added Gen AI Service Deep Dive Quiz

---
+++ gen_ai_service_deep_dive.py
@@ -0,0 +1,68 @@
+'''
+
+1) What happens if a period (.) is used as a stop sequence in text generation?
+
+A. The model stops generating text after it reaches the end of the first sentence, even if the token limit is much higher.
+
+B. The model generates additional sentences to complete the paragraph.
+
+C. The model ignores periods and continues generating text until it reaches the token limit.
+
+D. The model stops generating text after it reaches the end of the current paragraph.
+
+--> A
+
+
+2) What is the purpose of embeddings in natural language processing?
+
+A. To create numerical representations of text that capture the meaning and relationships between words or phrases
+
+B. To increase the complexity and size of text data
+
+C. To compress text data into smaller files for storage
+
+D. To translate text into a different language
+
+--> A
+
+
+3) What is the purpose of frequency penalties in language model outputs?
+
+A. To ensure that tokens that appear frequently are used more often
+
+B. To reward the tokens that have never appeared in the text
+
+C. To randomly penalize some tokens to increase the diversity of the text
+
+D. To penalize tokens that have already appeared, based on the number of times they have been used
+
+--> D
+
+
+4) Which is a distinctive feature of GPUs in Dedicated AI Clusters used for generative AI tasks?
+
+A. GPUs are shared with other customers to maximize resource utilization.
+
+B. The GPUs allocated for a customer’s generative AI tasks are isolated from other GPUs.
+
+C. GPUs are used exclusively for storing large data sets, not for computation.
+
+D. Each customer's GPUs are connected via a public Internet network for ease of access.
+
+--> B
+
+
+5) What is the main advantage of using few-shot model prompting to customize a Large Language Model (LLM)?
+
+A. It eliminates the need for any training or computational resources.
+
+B. It significantly reduces the latency for each model request.
+
+C. It allows the LLM to access a larger data set.
+
+D. It provides examples in the prompt to guide the LLM to better performance with no training cost.
+
+--> D
+
+
+'''
